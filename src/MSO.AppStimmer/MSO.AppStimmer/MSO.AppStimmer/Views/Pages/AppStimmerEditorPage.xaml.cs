0a74ab6fd04859488120b8879cf104393e18df88
Marvin Reiter <disappeared57@hotmail.com>
2017-11-23 08:50:38 +0100

    Added TabbedPage for AppStimmerEditor

--- src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/Views/Pages/AppStimmerEditorPage.xaml.cs
+++ src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/Views/Pages/AppStimmerEditorPage.xaml.cs
@@ -1,14 +1,27 @@
-﻿using Xamarin.Forms;
+﻿using MSO.StimmApp.Views.Controls;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MSO.StimmApp.Views.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AppStimmerEditorPage : ContentPage
+    public partial class AppStimmerEditorPage : TabbedPage
     {
         public AppStimmerEditorPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            //this.Children.Add(new ContentPage
+            //{
+            //    Title = "Overview",
+            //    Content = new AppStimmerEditorOverviewView()
+            //});
+
+            //this.Children.Add(new ContentPage
+            //{
+            //    Title = "Details",
+            //    Content = new AppStimmerEditorDetailsView()
+            //});
         }
     }
 }
