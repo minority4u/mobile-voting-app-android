ea067839d8c0327a9d9f9c3c692cfd9b8760fa77
Marvin Reiter <disappeared57@hotmail.com>
2017-11-28 22:45:08 +0100

    Implemented AppStimmersListView

--- src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/ViewModels/AppStimmersViewModel.cs
+++ src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/ViewModels/AppStimmersViewModel.cs
@@ -1,9 +1,28 @@
-﻿using MSO.StimmApp.Core.ViewModels;
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Ioc;
+using MSO.StimmApp.Core.Models;
+using MSO.StimmApp.Core.Services;
+using MSO.StimmApp.Core.ViewModels;
 
 namespace MSO.StimmApp.ViewModels
 {
     public class AppStimmersViewModel : BaseViewModel
     {
+        private readonly IAppStimmerService appStimmerService;
+        private ObservableCollection<AppStimmer> appStimmers;
 
+        [PreferredConstructor]
+        public AppStimmersViewModel(IAppStimmerService appStimmerService)
+        {
+            this.appStimmerService = appStimmerService;
+            this.appStimmers = new ObservableCollection<AppStimmer>(this.appStimmerService.GetAllAppStimmers());
+            //this.appStimmers.Shuffle();
+        }
+
+        public ObservableCollection<AppStimmer> AppStimmers
+        {
+            get => this.appStimmers;
+            set => this.Set(ref this.appStimmers, value);
+        }
     }
 }
