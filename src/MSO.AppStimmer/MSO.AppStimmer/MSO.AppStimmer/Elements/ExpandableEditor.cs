b8b6e6dcf7b2664cf7a6634345b75d9e2abd9601
Marvin Reiter <disappeared57@hotmail.com>
2017-12-11 11:44:26 +0100

    Improved AppStimmerDetailsView

---
+++ src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/Elements/ExpandableEditor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MSO.StimmApp.Elements
+{
+    public class ExpandableEditor : Editor
+    {
+        public ExpandableEditor()
+        {
+            this.TextChanged += (sender, e) => { this.InvalidateMeasure(); };
+        }
+    }
+}
