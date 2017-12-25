311c11308b2a1e80a08dfb92fadbc8662d11fabb
Marvin Reiter <disappeared57@hotmail.com>
2017-12-25 23:43:38 +0100

    dev

--- src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/Views/ContentViews/AppStimmerEditor/AppStimmerEditorDetailsView_2.xaml.cs
+++ src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/Views/ContentViews/AppStimmerEditor/AppStimmerEditorDetailsView_2.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MSO.Common;
 using MSO.StimmApp.ViewModels;
 using MSO.StimmApp.Views.Pages;
 using Rg.Plugins.Popup.Services;
@@ -14,11 +16,14 @@ namespace MSO.StimmApp.Views.ContentViews.AppStimmerEditor
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AppStimmerEditorDetailsView_2 : ContentView
     {
-        private double _imageHeight = 0;
+        private double imageHeight = 0;
+
+        private string baseTitleBarColor;
 
         public AppStimmerEditorDetailsView_2()
         {
             this.InitializeComponent();
+            this.baseTitleBarColor = App.Settings.AppColors.TitleBarColor;
         }
 
         public AppStimmerEditorViewModel ViewModel => this.BindingContext as AppStimmerEditorViewModel;
@@ -31,33 +36,94 @@ namespace MSO.StimmApp.Views.ContentViews.AppStimmerEditor
             await PopupNavigation.PushAsync(page);
         }
 
+        private void MainScrollView_OnScrolled(object sender, ScrolledEventArgs e)
+        {
+            this.Parallax();
+        }
+
         private void Parallax()
+        {
+            this.AdjustMainPicture();
+            this.AdjustTitleBarColor();
+            //this.AdjustMainPictureOverlay();
+        }
+
+        private void AdjustTitleBarColor()
         {
             var scrollView = this.MainScrollView;
             var photoImage = this.MainPicture;
 
-            if (_imageHeight <= 0)
-                _imageHeight = photoImage.Height;
+            var scrolled = scrollView.ScrollY;
+            var imgHeight = photoImage.Height;
+
+            var percentageScrolled = scrolled / imgHeight;
+
+            var settings = App.Settings.AppColors;
+            //var cloned = settings.CloneJson();
+
+            //if (percentageScrolled > 0.5)
+            //{
+            //    cloned.TitleBarColor = "#000000FF";             
+            //}
+            //else
+            //{
+            //    cloned.TitleBarColor = this.baseTitleBarColor;
+            //}
+
+            //App.Settings.AppColors = cloned;
+        }
+
+
+        private void AdjustMainPictureOverlay()
+        {
+            var scrollView = this.MainScrollView;
+            var overlayFrame = this.MainPictureOverlayFrame;
+            var photoImage = this.MainPicture;
+
+            var scrolled = scrollView.ScrollY;
+            var imgHeight = photoImage.Height;
+
+            var percentageScrolled = scrolled / imgHeight;
+
+            //Debug.WriteLine("Percentage scrolled: " + percentageScrolled);
+            //overlayFrame.Opacity = percentageScrolled;
+        }
+
+        //private void Parallax2()
+        //{
+        //    var image = this.MainnPicture;
+        //    var imageHeight = image.Height * 3;
+        //    var scrollRegion = grid.Height - scrollView.Height;
+        //    var parallexRegion = imageHeight - scrollView.Height;
+        //    image.TranslationY = scrollView.ScrollY - parallexRegion * (scrollView.ScrollY / scrollRegion);
+        //}
+
+        private void AdjustMainPicture()
+        {
+            var scrollView = this.MainScrollView;
+            var photoImage = this.MainPicture;
+            var title = this.TitleFrame;
+
+            if (this.imageHeight <= 0)
+                this.imageHeight = photoImage.Height;
 
             var y = scrollView.ScrollY * .4;
+
             if (y >= 0)
             {
                 //Move the Image's Y coordinate a fraction of the ScrollView's Y position
                 photoImage.Scale = 1;
                 photoImage.TranslationY = y;
+                title.TranslationY = -y;
             }
             else
             {
-                //Calculate a scale that equalizes the height vs scroll
-                double newHeight = _imageHeight + (scrollView.ScrollY * -1);
-                photoImage.Scale = newHeight / _imageHeight;
+                Debug.WriteLine("Else");
+                ////Calculate a scale that equalizes the height vs scroll
+                double newHeight = this.imageHeight + (scrollView.ScrollY * -1);
+                photoImage.Scale = newHeight / this.imageHeight;
                 photoImage.TranslationY = scrollView.ScrollY / 2;
             }
-        }
-
-        private void MainScrollView_OnScrolled(object sender, ScrolledEventArgs e)
-        {
-            this.Parallax();
         }
     }
 }
