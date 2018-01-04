dfe4997101c32e5843a35c4f0c776da51f004df0
minority4u <emailsvenkoehler@gmx.net>
2018-01-04 12:46:35 +0100

    added new splashscreen

--- src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer.Android/Resources/Resource.designer.cs
+++ src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer.Android/Resources/Resource.designer.cs
@@ -7491,26 +7491,26 @@ namespace MSO.StimmApp.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020129
-			public const int avd_hide_password_1 = 2130837801;
-			
-			// aapt resource value: 0x7f02012a
-			public const int avd_hide_password_2 = 2130837802;
-			
 			// aapt resource value: 0x7f02012b
-			public const int avd_hide_password_3 = 2130837803;
+			public const int avd_hide_password_1 = 2130837803;
+			
+			// aapt resource value: 0x7f02012c
+			public const int avd_hide_password_2 = 2130837804;
+			
+			// aapt resource value: 0x7f02012d
+			public const int avd_hide_password_3 = 2130837805;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02012c
-			public const int avd_show_password_1 = 2130837804;
-			
-			// aapt resource value: 0x7f02012d
-			public const int avd_show_password_2 = 2130837805;
-			
 			// aapt resource value: 0x7f02012e
-			public const int avd_show_password_3 = 2130837806;
+			public const int avd_show_password_1 = 2130837806;
+			
+			// aapt resource value: 0x7f02012f
+			public const int avd_show_password_2 = 2130837807;
+			
+			// aapt resource value: 0x7f020130
+			public const int avd_show_password_3 = 2130837808;
 			
 			// aapt resource value: 0x7f020056
 			public const int calendar_bg_selector = 2130837590;
@@ -8118,11 +8118,11 @@ namespace MSO.StimmApp.Droid
 			// aapt resource value: 0x7f02011f
 			public const int notification_icon_background = 2130837791;
 			
-			// aapt resource value: 0x7f020127
-			public const int notification_template_icon_bg = 2130837799;
+			// aapt resource value: 0x7f020129
+			public const int notification_template_icon_bg = 2130837801;
 			
-			// aapt resource value: 0x7f020128
-			public const int notification_template_icon_low_bg = 2130837800;
+			// aapt resource value: 0x7f02012a
+			public const int notification_template_icon_low_bg = 2130837802;
 			
 			// aapt resource value: 0x7f020120
 			public const int notification_tile_bg = 2130837792;
@@ -8143,7 +8143,13 @@ namespace MSO.StimmApp.Droid
 			public const int splash_screen = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int thumbs_up = 2130837798;
+			public const int splashscreen = 2130837798;
+			
+			// aapt resource value: 0x7f020127
+			public const int splashscreen_small = 2130837799;
+			
+			// aapt resource value: 0x7f020128
+			public const int thumbs_up = 2130837800;
 			
 			static Drawable()
 			{
