baa207fa39cfdc944ab81857568e13a68b5cf52e
Marvin Reiter <disappeared57@hotmail.com>
2018-01-07 14:20:24 +0100

    Fixed a few more bugs

---
+++ src/MSO.AppStimmer/MSO.AppStimmer/MSO.AppStimmer/Converter/DescriptionAvailabilityToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSO.StimmApp.Converter
+{
+    public class DescriptionAvailabilityToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] == null)
+                return false;
+
+            var isNew = (bool)values[0];
+            var description = values[1]?.ToString();
+            
+            if (isNew)
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
